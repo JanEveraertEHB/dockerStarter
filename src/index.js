664d2fdd7bc18ecc251164e5e5a38369f51617fb
Jan Everaert <jan@crashlab.be>
2021-10-08 10:42:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const server = express();
 const PORT = 3000;
 
 const owners = [{
-  name: "Jan Everaert",
-  email: "jan.everaert@ehb.be"
+  name: "hello",
+  email: "hello@ehb.be"
 }];
 
 /**
@@ -23,38 +23,6 @@ server.get("/", (req, res) => {
 server.get("/owner", (req, res) => {
   res.json(owner);
 })
-
-
-/**
- * @typedef ownerObject
- * @property {string} name the name of the new owner 
- * @property {string} email the email address of the new owner 
- */
-/**
- * [POST] /owner
- * @param {ownerObject} owner containing an owner object
- * @returns 200 if succesfully added
- */
-
-// -----------------------
-/**
- * [POST] /owner
- * @param {String} owner.name name of the new owner
- * @param {String} owner.email email of the new owner
- * @returns 200 if succesfully added
- */
-
-
-server.post("/owner", (req, res) => {
-  if(req.body.name && req.body.email) {
-    owners.push(req.body);
-    res.status(200).send();
-  }
-  else {
-    res.status(400).send();
-  }
-});
-
 
 server.listen(PORT, () => {
   console.log(`server listening at port ${PORT}`);
